e9241f794e80861d5d215ba8f5d4031692d202a3
thanhbinh <thanhbinh27102003@gmail.com>
2023-05-06 09:36:00 +0700

    HOME VS LOGIN

--- src/VIEW/LOGIN.java
+++ src/VIEW/LOGIN.java
@@ -20,7 +20,7 @@ public class LOGIN extends javax.swing.JFrame {
      */
     public LOGIN() {
         initComponents();
-        ImageIcon myImage =new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("cocaro.png")));
+        ImageIcon myImage =new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("cocaro.jpq")));
         Image img1 = myImage.getImage();
         Image img2 = img1.getScaledInstance(anhnen.getWidth(), anhnen.getHeight(),Image.SCALE_SMOOTH);
         ImageIcon i = new ImageIcon(img2);
